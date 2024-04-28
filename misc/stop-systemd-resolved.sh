99c0a9b211070ef6e391a59f0d680211d9573bd5
M <38591075+M507@users.noreply.github.com>
2024-04-27 20:43:09 -0400

    Update stop-systemd-resolved.sh

--- misc/stop-systemd-resolved.sh
+++ misc/stop-systemd-resolved.sh
@@ -1,3 +1,4 @@
+# https://medium.com/@niktrix/getting-rid-of-systemd-resolved-consuming-port-53-605f0234f32f
 sudo systemctl stop systemd-resolved
 sudo vim /etc/systemd/resolved.conf
 sudo ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
